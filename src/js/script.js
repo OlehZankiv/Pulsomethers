ade9a6813cfd78df7f65edecf635d6304a6c4cab
Oleh Zankic <firegames5645@gmail.com>
2020-03-24 14:39:53 +0200

    Create and adaptive section(rewiews)

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 window.onload = () => {
+    //SLIDER
     var slider = tns({
         container: '.carousel__inner',
         items: 1,
@@ -15,4 +16,53 @@ window.onload = () => {
     document.querySelector('.next-arrow').addEventListener("click", () => {
         slider.goTo("next");
     });
+
+    //TABS
+    let tabBtnContainer = document.querySelector(".catalog__tabs"),
+        tabBtns = document.querySelectorAll(".catalog__tab"),
+        tabs = document.querySelectorAll(".catalog__content");
+
+    function showTab(tab) {
+        tab.classList.toggle("catalog__content_active");
+    }
+
+    function hideTabs() {
+        tabs.forEach((elem) => {
+            elem.classList.remove("catalog__content_active");
+        });
+    }
+
+    function toggleActiveBtn(btn) {
+        tabBtns.forEach((elem) => {
+            elem.classList.remove("catalog__tab_active");
+        });
+        btn.classList.add("catalog__tab_active");
+    }
+    tabBtnContainer.addEventListener("click", (event) => {
+        tabBtns.forEach((elem, i) => {
+            if (elem === event.target) {
+                toggleActiveBtn(elem);
+                hideTabs();
+                showTab(tabs[i]);
+            }
+        });
+    });
+
+    let left = document.querySelectorAll(".catalog-item__content"),
+        right = document.querySelectorAll(".catalog-item__list");
+
+    toggleItemDescr(".catalog-item__link");
+    toggleItemDescr(".catalog-item__back");
+
+    function toggleItemDescr(btnAll) {
+        btns = document.querySelectorAll(btnAll);
+        btns.forEach((item, i) => {
+            item.addEventListener("click", function (event) {
+                event.preventDefault();
+                left[i].classList.toggle("catalog-item__content_active");
+                right[i].classList.toggle("catalog-item__list_active");
+            });
+        });
+
+    }
 };
